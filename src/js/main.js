8d3a23f038038d82e6e3846d108f337ef7fcec2b
Anthony Halliday <anthony.halliday@fatmedia.co.uk>
2020-07-07 08:15:54 +0100

    Mapbox

--- src/js/main.js
+++ src/js/main.js
@@ -74,6 +74,14 @@ var swiper = new Swiper('.home-slider', {
     prevButton: '.home-slider-prev'
 });
 
+//further slider
+var swiper = new Swiper('.further-slider', {
+    pagination: '.further-pagination',
+    paginationClickable: true,
+    nextButton: '.further-slider-next',
+    prevButton: '.further-slider-prev'
+});
+
 //testimonials slider
 var swiper = new Swiper('.testimonials-slider', {
     pagination: '.testimonials-pagination',
@@ -160,107 +168,18 @@ $(".navbar-nav li a[href^='#']").on('click', function(e) {
 
 });
 
-// Google maps configuration
-// set your latitude, longitude and address of any point on Google Maps - http://www.gps-coordinates.net/
-// Google maps reference - https://developers.google.com/maps/
-// You need API key - https://developers.google.com/maps/documentation/javascript/get-api-key
-// place your api key in link to script  <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY"></script>
-var map;
-var mapAddress = new google.maps.LatLng(52.406374, 16.925168100000064);
+mapboxgl.accessToken = 'pk.eyJ1IjoiYW50aDEyIiwiYSI6ImNrY2FvdXE3ZjF4NDIyem8wcWhsNTM0eG0ifQ.RzZvSEyKhL4QDtdQknZeDA';
+var map = new mapboxgl.Map({
+    container: 'map-canvas',
+    //style: 'mapbox://styles/mapbox/streets-v11',
+    style: 'mapbox://styles/anth12/ckcaq0e9q5e931imt36gdee2n',
+    center: [-2.5,54.43],
+    zoom: 8.5
+  });
 
-function initialize() {
+  map.addControl(new mapboxgl.NavigationControl());
+  map.scrollZoom.disable();
 
-    var roadAtlasStyles = [{
-        "featureType": "road.highway",
-        "elementType": "geometry",
-        "stylers": [{
-            "saturation": -100
-        }, {
-            "lightness": -8
-        }, {
-            "gamma": 1.18
-        }]
-    }, {
-        "featureType": "road.arterial",
-        "elementType": "geometry",
-        "stylers": [{
-            "saturation": -100
-        }, {
-            "gamma": 1
-        }, {
-            "lightness": -24
-        }]
-    }, {
-        "featureType": "poi",
-        "elementType": "geometry",
-        "stylers": [{
-            "saturation": -100
-        }]
-    }, {
-        "featureType": "administrative",
-        "stylers": [{
-            "saturation": -100
-        }]
-    }, {
-        "featureType": "transit",
-        "stylers": [{
-            "saturation": -100
-        }]
-    }, {
-        "featureType": "water",
-        "elementType": "geometry.fill",
-        "stylers": [{
-            "saturation": -100
-        }]
-    }, {
-        "featureType": "road",
-        "stylers": [{
-            "saturation": -100
-        }]
-    }, {
-        "featureType": "administrative",
-        "stylers": [{
-            "saturation": -100
-        }]
-    }, {
-        "featureType": "landscape",
-        "stylers": [{
-            "saturation": -100
-        }]
-    }, {
-        "featureType": "poi",
-        "stylers": [{
-            "saturation": -100
-        }]
-    }, {}]
-
-
-    var mapOptions = {
-        zoom: 15,
-        scrollwheel: false,
-        center: mapAddress,
-        mapTypeControlOptions: {
-            mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'usroadatlas']
-        }
-    };
-
-    map = new google.maps.Map(document.getElementById('map-canvas'),
-        mapOptions);
-
-    var styledMapOptions = {
-
-    };
-
-    var marker = new google.maps.Marker({
-        position: mapAddress,
-        map: map,
-    });
-
-    var usRoadMapType = new google.maps.StyledMapType(
-        roadAtlasStyles, styledMapOptions);
-
-    map.mapTypes.set('usroadatlas', usRoadMapType);
-    map.setMapTypeId('usroadatlas');
-}
-
-google.maps.event.addDomListener(window, 'load', initialize);
+  var marker = new mapboxgl.Marker();
+  marker.setLngLat([-2.3398478,54.4332042]);
+  marker.addTo(map);
